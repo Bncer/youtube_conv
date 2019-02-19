f239859d52559ebc8021e8561bd13cdfc0c9e0de
Bncer <nurseitkamchyev@gmail.com>
2019-02-20 01:16:49 +0600

    My html file connected to view of django and from urls main page will be without app name

--- Dj_youtube/Dj_youtube/urls.py
+++ Dj_youtube/Dj_youtube/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('convert/', include('convert.urls')),
+    path('', include('convert.urls')),
     path('admin/', admin.site.urls),
 ]
