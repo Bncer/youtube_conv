6ea461cba5064b77f36fc12dd57153bf1ea600a5
Bncer <nurseitkamchyev@gmail.com>
2019-02-19 19:11:28 +0600

    Password doc didn't work, used decouple. Add some code for youtube-dl

--- Dj_youtube/convert/models.py
+++ Dj_youtube/convert/models.py
@@ -1,10 +1,25 @@
+from __future__ import unicode_literals
 from django.db import models
+import youtube_dl
 
 # Create your models here.
+
 
 class History(models.Model):
     history_text = models.CharField(max_length=200)
 
+    def download_song(song_url):
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [
+                {'key': 'FFmpegExtractAudio',
+                 'preferredcodec': 'mp3',
+                 'preferredquality': '192'},
+            ],
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([song_url])
+
     def __str__(self):
         return self.history_text
-
