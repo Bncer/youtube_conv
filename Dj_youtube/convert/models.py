888bc39b7a9092b663ba9eeff9367ca3d07bc9f5
Bncer <nurseitkamchyev@gmail.com>
2019-02-21 01:00:19 +0600

    Added two functions(get, post) to convert request. In the model function saving the history

--- Dj_youtube/convert/models.py
+++ Dj_youtube/convert/models.py
@@ -8,7 +8,14 @@ import youtube_dl
 class History(models.Model):
     history_text = models.CharField(max_length=200)
 
-    def download_song(song_url):
+    def __str__(self):
+        return self.history_text
+
+    def save_to_db(self, url_to_save):
+        q = History(history_text=url_to_save)
+        q.save()
+
+    def download_song(self, song_url):
         ydl_opts = {
             'format': 'bestaudio/best',
             'noplaylist' : True,
@@ -22,6 +29,4 @@ class History(models.Model):
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([song_url])
 
-    def __str__(self):
-        return self.history_text
 
--- Dj_youtube/convert/views.py
+++ Dj_youtube/convert/views.py
@@ -5,18 +5,28 @@ from django.views import generic
 # Create your views here.
 
 
-class IndexView(generic.ListView):
-    template_name = 'convert/index.html'
-    context_object_name = 'all_history_list'
+class IndexView(generic.TemplateView):
 
-    def get_queryset(self):
-        return History.objects.all()
+
+    def get(self, request):
+        template_name = 'convert/index.html'
+        return render(request, template_name)
+
+    def post(self, request):
+        print(request.POST)
+        urls = request.POST['url']
+        History.download_song(urls, urls)
+        History.save_to_db(urls, urls)
+        return HttpResponse("Good")
 
 
 class HistoryView(generic.ListView):
     model = History
     template_name = 'convert/history_list.html'
+    context_object_name = 'all_history_list'
 
+    def get_queryset(self):
+        return History.objects.all()
 
 
 
